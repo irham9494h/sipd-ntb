3144090210f064f9b13d00945a1b1d2e8c7a37fc
Ahmad Irham <irham9494h@gmail.com>
2021-03-01 11:20:22 +0800

    update kepala OPD/UPT

--- app/contents/rka-skpd.js
+++ app/contents/rka-skpd.js
@@ -1,10 +1,23 @@
-var tableTr = $('table[cellpadding="2"]').parent().find('tr td.text_tengah').first()
+var tableTr = $('table[cellpadding="2"]').parent().find('tr td.text_tengah')
 
 if(current_url.indexOf('rinci/cetak') != -1){
-    var text = tableTr.text()
+    tableTr.first().text()
     
-    chrome.storage.sync.get(['tanggalTtd'], function (object) {
-        tableTr.text("Mataram, tanggal "+object.tanggalTtd)
+    chrome.storage.sync.get(['tanggalTtd', 'jabatanKadis', 'namaKadis', 'nipKadis'], 
+    function (object) {
+        tableTr.first().text("Mataram, tanggal "+object.tanggalTtd)
+        
+        if(object.jabatanKadis !== '' || object.namaKadis !== '' || object.nipKadis !== ''){
+            tableTr.eq(1).text()
+            tableTr.eq(2).text()
+            tableTr.last().text()
+            
+            tableTr.eq(1).text(object.jabatanKadis)
+            tableTr.eq(2).text(object.namaKadis)
+            tableTr.last().text("NIP. "+object.nipKadis)
+        }
+        
     });
+    
 }
 
